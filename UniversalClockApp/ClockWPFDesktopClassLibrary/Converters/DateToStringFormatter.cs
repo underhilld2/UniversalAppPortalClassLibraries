3b7605029cb7d14fbc8f45e54bc3f853a039038a
David Underhill <d.underhill@techsmith.com>
2014-10-03 18:19:10 -0400

    Added converter

--- UniversalClockApp/ClockWPFDesktopClassLibrary/Converters/DateToStringFormatter.cs
+++ UniversalClockApp/ClockWPFDesktopClassLibrary/Converters/DateToStringFormatter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Globalization;
+using System.Windows.Data;
 
 
 namespace XamlHelpers.Converters
@@ -12,6 +13,16 @@ namespace XamlHelpers.Converters
       }
 
       public object ConvertBack( object value, Type targetType, object parameter, string language )
+      {
+         throw new NotImplementedException();
+      }
+
+      public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+      {
+         throw new NotImplementedException();
+      }
+
+      public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
       {
          throw new NotImplementedException();
       }
