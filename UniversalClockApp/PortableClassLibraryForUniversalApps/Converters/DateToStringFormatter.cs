9dd065425d3523331edee6629e498989ca153a88
David Underhill <d.underhill@techsmith.com>
2014-10-03 18:03:59 -0400

    Added dateToStringFormatter

---
+++ UniversalClockApp/PortableClassLibraryForUniversalApps/Converters/DateToStringFormatter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+
+namespace XamlHelpers.Converters
+{
+   public sealed class DateToStringFormatter : IValueConverter
+   {
+      public object Convert( object value, Type targetType, object parameter, string language )
+      {
+         return DateToStringCommonCode.Convert(value, targetType, parameter, language);
+      }
+
+      public object ConvertBack( object value, Type targetType, object parameter, string language )
+      {
+         throw new NotImplementedException();
+      }
+   }
+
+
+}
