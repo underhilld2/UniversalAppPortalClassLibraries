78af167d13160087706fc2009b154d81499c82ad
David Underhill <d.underhill@techsmith.com>
2014-10-03 18:18:35 -0400

    Added WPF class library

--- UniversalClockApp/PortableClassLibraryForDotNet40/Converters/DateToStringFormatter.cs
+++ UniversalClockApp/PortableClassLibraryForDotNet40/Converters/DateToStringFormatter.cs
@@ -6,41 +6,55 @@ namespace XamlHelpers.Converters
 {
    public sealed class DateToStringFormatter : IValueConverter
    {
+      //public object Convert( object value, Type targetType, object parameter, string language )
+      //{
+      //   if ( value is DateTime )
+      //   {
+      //      DateTime dt = (DateTime) value;
+
+      //      if ( parameter is string )
+      //      {
+      //         switch ( ( parameter as string ).ToLower() )
+      //         {
+      //            case "datetime":
+      //            return string.Format( "{0:MMMM dd, yyyy - h:mm tt}", dt );
+      //            case "date":
+      //            return string.Format( "{0:MMMM dd, yyyy}", dt );
+      //            case "short":
+      //            return string.Format( " {0:MM/dd/yyyy}", dt );
+      //            case "time":
+      //            return string.Format( " {0:T}", dt );
+      //            case "hour":
+      //            return string.Format( " {0:hh}", dt );
+      //            case "minute":
+      //            return string.Format( " {0:mm}", dt );
+      //         }
+      //      }
+
+      //      return dt.ToString();
+      //   }
+
+      //   return "";
+      //}
+
+      //public object ConvertBack( object value, Type targetType, object parameter, string language )
+      //{
+      //   throw new NotImplementedException();
+      //}
+
+
+
       public object Convert( object value, Type targetType, object parameter, string language )
       {
-         if ( value is DateTime )
-         {
-            DateTime dt = (DateTime) value;
-
-            if ( parameter is string )
-            {
-               switch ( ( parameter as string ).ToLower() )
-               {
-                  case "datetime":
-                  return string.Format( "{0:MMMM dd, yyyy - h:mm tt}", dt );
-                  case "date":
-                  return string.Format( "{0:MMMM dd, yyyy}", dt );
-                  case "short":
-                  return string.Format( " {0:MM/dd/yyyy}", dt );
-                  case "time":
-                  return string.Format( " {0:T}", dt );
-                  case "hour":
-                  return string.Format( " {0:hh}", dt );
-                  case "minute":
-                  return string.Format( " {0:mm}", dt );
-               }
-            }
-
-            return dt.ToString();
-         }
-
-         return "";
+         return DateToStringCommonCode.Convert( value, targetType, parameter, language );
       }
 
       public object ConvertBack( object value, Type targetType, object parameter, string language )
       {
          throw new NotImplementedException();
       }
+
+
    }
 
 
--- UniversalClockApp/PortableClassLibraryForDotNet451/Converters/DateToStringFormatter.cs
+++ UniversalClockApp/PortableClassLibraryForDotNet451/Converters/DateToStringFormatter.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Windows.UI.Xaml.Data;
+
 
 
 namespace XamlHelpers.Converters
@@ -8,33 +8,7 @@ namespace XamlHelpers.Converters
    {
       public object Convert( object value, Type targetType, object parameter, string language )
       {
-         if ( value is DateTime )
-         {
-            DateTime dt = (DateTime) value;
-
-            if ( parameter is string )
-            {
-               switch ( ( parameter as string ).ToLower() )
-               {
-                  case "datetime":
-                  return string.Format( "{0:MMMM dd, yyyy - h:mm tt}", dt );
-                  case "date":
-                  return string.Format( "{0:MMMM dd, yyyy}", dt );
-                  case "short":
-                  return string.Format( " {0:MM/dd/yyyy}", dt );
-                  case "time":
-                  return string.Format( " {0:T}", dt );
-                  case "hour":
-                  return string.Format( " {0:hh}", dt );
-                  case "minute":
-                  return string.Format( " {0:mm}", dt );
-               }
-            }
-
-            return dt.ToString();
-         }
-
-         return "";
+         return DateToStringCommonCode.Convert( value, targetType, parameter, language );
       }
 
       public object ConvertBack( object value, Type targetType, object parameter, string language )
